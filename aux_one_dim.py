f08f5a4dbd114ce65e1d22dcd30aaa033782eb9e
pawel <pawel202@protonmail.com>
2020-05-16 22:20:23 +0200

    one dimension corrections

--- aux_one_dim.py
+++ aux_one_dim.py
@@ -2,20 +2,20 @@ from numpy import sign
 import numpy as np
 from numpy import gradient
 
+#extrapolation to value^{t+0.5}
 def extrapolate_in_time(u_old, u_new):
-    return (u_old + u_new) * 1.5
+    return (u_new + u_old) * .5
 
 def grad(mass):
-    l = gradient(mass[0])
-    l = list([l])
-    return np.array(l)
+    return gradient(mass)
 
 
 
 def interpolate_in_space(uh, h):
-    u = np.zeros((uh.shape[0], uh.shape[1] + 1))
+    u = np.zeros((uh.shape[0] + 1))
+    result = uh/h
 
-    for i in range(uh.shape[1] - 1):
-        u[0][i+1] = ( uh[0][i] + h[0][i]) / 2.
+    for i in range(1, u.shape[0] - 1):
+        u[i] = (result[i-1] + result[i]) / 2.
 
     return u 
