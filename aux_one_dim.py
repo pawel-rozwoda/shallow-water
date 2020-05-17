0a78401aeb48dda0dadd676b5b7f6181ad1c155d
pawel <pawel202@protonmail.com>
2020-05-17 23:04:24 +0200

    MPDATA function

--- aux_one_dim.py
+++ aux_one_dim.py
@@ -2,19 +2,25 @@ from numpy import sign
 import numpy as np
 from numpy import gradient
 
+
 #extrapolation to value^{t+0.5}
 def extrapolate_in_time(u_old, u_new):
-    return (u_new + u_old) * .5
+    return (3. * u_new - u_old) * .5
 
-def grad(mass):
-    return gradient(mass)
 
+def grad(mass, dx=1.):
+    return gradient(mass) / dx
 
 
 def interpolate_in_space(uh, h):
     u = np.zeros((uh.shape[0] + 1))
-    result = uh/h
 
+    try:
+        result = np.where(h > 0, uh/h, 0.)
+        result = np.where(h < 0 , 0, result)
+    except:
+        pass
+    
     for i in range(1, u.shape[0] - 1):
         u[i] = (result[i-1] + result[i]) / 2.
 
